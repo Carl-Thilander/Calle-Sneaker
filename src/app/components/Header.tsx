2eb45fa75d5d1399dcd3cc4f1d2a5d0565b14bbd
Carl Thilander <carl.thilander@medieinstitutet.se>
2025-11-13 14:17:41 +0100

    Updated header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,24 +1,58 @@
 'use client';
-import { AppBar, Toolbar, Typography, Button, Box } from '@mui/material';
+
+
+import { AppBar, Toolbar, Typography, Button, Box, Container } from '@mui/material';
 import Link from 'next/link';
 
 export default function Header() {
     return (
-        <>
-            <AppBar position="sticky" elevation={0} sx={{ borderBottom: "2px solid black", height: "10vh", display: "flex", justifyContent: "center" }} color="inherit">
-                <Toolbar sx={{ gap: 5 }}>
-                    <Typography variant="h1" sx={{ ml: 20, flexGrow: 0.3 }}>Sneakers</Typography>
-                    <Box sx={{ display: 'flex', gap: 8, flexGrow: 0.2, justifyContent: 'center' }}>
-                        <Typography variant="h4" component={Link} sx={{ color: "ActiveCaption", textDecoration: "none" }} href="/inspired">Get Inspired</Typography>
-                        <Typography variant="h4" component={Link} sx={{ color: "ActiveCaption", textDecoration: "none" }} href="/designer">Customize</Typography>
-                        <Typography variant="h4" component={Link} sx={{ color: "ActiveCaption", textDecoration: "none" }} href="/about">About</Typography>
-                    </Box>
-                    <Box sx={{ display: 'flex', gap: 1, flexGrow: 0.3, justifyContent: 'flex-end', mr: 20 }}>
-                        <Button variant="contained" component={Link} href="/login">Log in</Button>
+        <AppBar
+            position="sticky"
+            elevation={0}
+            color="inherit"
+            sx={{
+                borderBottom: "2px solid black",
+                height: "10vh",
+                display: "flex",
+                justifyContent: "center"
+            }}
+        >
+            <Container maxWidth="xl">
+                <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
 
+                    {/* LOGGA */}
+                    <Typography
+                        variant="h1"
+                        component={Link}
+                        href="/"
+                        sx={{
+                            textDecoration: "none",
+                            color: "black",
+                        }}
+                    >
+                        Sneakers
+                    </Typography>
+
+                    {/* MENY */}
+                    <Box sx={{ display: "flex", gap: 6 }}>
+                        <Typography variant="h4" component={Link} href="/inspired" sx={{ textDecoration: "none", color: "ActiveCaption" }}>
+                            Get Inspired
+                        </Typography>
+                        <Typography variant="h4" component={Link} href="/designer" sx={{ textDecoration: "none", color: "ActiveCaption" }}>
+                            Customize
+                        </Typography>
+                        <Typography variant="h4" component={Link} href="/about" sx={{ textDecoration: "none", color: "ActiveCaption" }}>
+                            About
+                        </Typography>
                     </Box>
+
+                    {/* LOGIN */}
+                    <Button variant="contained" component={Link} href="/login">
+                        Log in
+                    </Button>
+
                 </Toolbar>
-            </AppBar >
-        </>
+            </Container>
+        </AppBar>
     );
 }
