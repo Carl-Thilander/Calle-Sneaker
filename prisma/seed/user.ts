7bd0506a47d372de2595cdb08c8a9e8e9115af45
carl-thilander <carl.thilander@medieinstitutet.se>
2025-11-17 15:39:52 +0100

    created prisma.config

--- prisma/seed/user.ts
+++ prisma/seed/user.ts
@@ -1,4 +1,4 @@
-import { User } from "@/generated/prisma";
+import { User } from "@/generated/client";
 import { db } from "../db";
 
 export async function seedDefaultUser() {
@@ -7,14 +7,16 @@ export async function seedDefaultUser() {
       email: "mockeduser@example.com",
       name: "Mocked User",
       password: "password123",
+      id: "",
+      createdAt: new Date(),
+      updatedAt: new Date(),
     },
   ];
-}
-
-for (const userData of mockedUser) {
-  await db.user.upsert({
-    where: { email: userData.email },
-    update: {},
-    create: userData,
-  });
+  for (const userData of mockedUser) {
+    await db.user.upsert({
+      where: { email: userData.email },
+      update: {},
+      create: userData,
+    });
+  }
 }
