f5ab71b8b43882c067f705e7be1c347595ea9aab
carl-thilander <carl.thilander@medieinstitutet.se>
2025-11-17 12:21:28 +0100

    With footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,28 +1,35 @@
 "use client";
 
-import { Toolbar, Button, Box } from "@mui/material";
+import { Box, Button, Toolbar } from "@mui/material";
 import Typography from "@mui/material/Typography";
 import Link from "next/link";
 
-
-
 export default function Footer() {
-    return (
-        <>
-
-            <Box component="footer" sx={{ mt: 4 }}>
-                <Toolbar sx={{
-                    gap: 2,
-                    backgroundColor: '#f5f5f5',
-                }}>
-                    <Typography variant="h6" sx={{ flexGrow: 1 }}>CustomKicks</Typography>
-                    <Box sx={{ display: 'flex', gap: 1 }}>
-                        <Button component={Link} href="/">Hem</Button>
-                        <Button component={Link} href="/inspired">Get Inspired</Button>
-                        <Button variant="contained" component={Link} href="/designer">Designa sko</Button>
-                    </Box>
-                </Toolbar>
-            </Box>
-        </>
-    );
+  return (
+    <>
+      <Box component="footer" sx={{ mt: 4 }}>
+        <Toolbar
+          sx={{
+            gap: 2,
+            backgroundColor: "#f5f5f5",
+          }}
+        >
+          <Typography variant="h6" sx={{ flexGrow: 1 }}>
+            CustomKicks
+          </Typography>
+          <Box sx={{ display: "flex", gap: 1 }}>
+            <Button component={Link} href="/">
+              Hem
+            </Button>
+            <Button component={Link} href="/inspired">
+              Get Inspired
+            </Button>
+            <Button variant="contained" component={Link} href="/designer">
+              Designa sko
+            </Button>
+          </Box>
+        </Toolbar>
+      </Box>
+    </>
+  );
 }
