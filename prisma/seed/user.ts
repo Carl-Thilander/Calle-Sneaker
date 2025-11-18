c571c99434e493e380030a18730f3fb2c2c0da4e
carl-thilander <carl.thilander@medieinstitutet.se>
2025-11-18 10:33:33 +0100

    refined seed command and succesfully exported mocked users to database

--- prisma/seed/user.ts
+++ prisma/seed/user.ts
@@ -1,22 +1,28 @@
-import { User } from "@/generated/client";
 import { db } from "../db";
 
 export async function seedDefaultUser() {
-  const mockedUser: User[] = [
+  const mockedUser = [
     {
       email: "mockeduser@example.com",
       name: "Mocked User",
       password: "password123",
-      id: "",
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      email: "anothermocked@email.se",
+      name: "Another Mocked",
+      password: "securepassword",
       createdAt: new Date(),
       updatedAt: new Date(),
     },
   ];
+
   for (const userData of mockedUser) {
     await db.user.upsert({
       where: { email: userData.email },
       update: {},
-      create: userData,
+      create: userData as any,
     });
   }
 }
