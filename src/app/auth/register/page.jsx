34c4aa255e99e45e2579b9448d55e05bbfe74369
carl-thilander <carl.thilander@medieinstitutet.se>
2025-12-05 11:25:12 +0100

    Sketch of Register page

---
+++ src/app/auth/register/page.jsx
@@ -0,0 +1,39 @@
+import { Box, Button, Container, Link, TextField } from "@mui/material";
+import AuthCard from "../components/AuthCard";
+
+export default function RegisterPage(){
+    return (
+        <Container maxWidth="lg" sx={{
+            mt: 4,
+        }}>
+
+        <AuthCard title="Create your account below" subtitle="">
+              <Box
+                component="form"
+                sx={{ display: "flex", flexDirection: "column", gap: 2, mt: 2 }}
+              >
+                <TextField label="Name" type="text" fullWidth required />
+                <TextField label="Email" type="email" fullWidth required />
+                <TextField label="Password" type="password" fullWidth required />
+                <TextField label="Re-enter your password" type="password" fullWidth required />
+        
+                <Button
+                  type="submit"
+                  variant="contained"
+                  color="primary"
+                  fullWidth
+                  sx={{ mt: 2 }}
+                >
+                  Create Acount
+                </Button>
+        
+                <Box textAlign="center" mt={2}>
+                  <Link href="/auth/register" style={{ textDecoration: "none" }}>
+                    Dont't have an account yet? Create one here!
+                  </Link>
+                </Box>
+              </Box>
+            </AuthCard>
+        </Container>
+    )
+}
