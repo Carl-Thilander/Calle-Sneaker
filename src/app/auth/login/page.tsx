87ed59bd262ea509cf2550463bad2d001d69a910
carl-thilander <carl.thilander@medieinstitutet.se>
2025-12-05 11:04:41 +0100

    First UI created for login

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,75 +1,32 @@
-import { Box, Container, Typography } from "@mui/material";
-import Image from "next/image";
-import loginSneaker from "../../assets/loginSneaker.jpg";
+import { Box, Button, Link, TextField } from "@mui/material";
+import AuthCard from "../components/AuthCard";
 
 export default async function LoginPage() {
   return (
-    <Container
-      maxWidth="lg"
-      sx={{
-        mt: 4,
-        py: { xs: 6, md: 10 },
-        px: { xs: 2, md: 4 },
-        backgroundColor: "paper.background",
-        borderRadius: 2,
-        boxShadow: 3,
-      }}
-    >
+    <AuthCard title="Welcome Back!" subtitle="Please log in to your account">
       <Box
-        sx={{
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "center",
-          width: "100%",
-          position: "relative",
-          height: "60vh",
-          gap: 4,
-        }}
+        component="form"
+        sx={{ display: "flex", flexDirection: "column", gap: 2, mt: 2 }}
       >
-        <Box
-          sx={{
-            width: "40%",
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "flex-start",
-            alignItems: "center",
-            px: 4,
-            backgroundColor: "background.paper",
-            borderRadius: 2,
-            boxShadow: 3,
-            gap: 2,
-            pt: 4,
-          }}
+        <TextField label="Email" type="email" fullWidth required />
+        <TextField label="Password" type="password" fullWidth required />
+
+        <Button
+          type="submit"
+          variant="contained"
+          color="primary"
+          fullWidth
+          sx={{ mt: 2 }}
         >
-          <Typography
-            variant="h3"
-            component="h1"
-            color="paper.background"
-            gutterBottom
-          >
-            Welcome back!
-          </Typography>
-          <Typography variant="h4" color="paper.background" gutterBottom>
-            Sign in to your account below
-          </Typography>
-        </Box>
-        <Box
-          sx={{
-            position: "relative",
-            width: "40%",
-            height: "auto",
-            boxSizing: "content-box",
-          }}
-        >
-          <Image
-            src={loginSneaker}
-            alt="Login Illustration"
-            fill
-            style={{ objectFit: "contain" }}
-            priority
-          />
+          Log In
+        </Button>
+
+        <Box textAlign="center" mt={2}>
+          <Link href="/auth/register" style={{ textDecoration: "none" }}>
+            Dont't have an account yet? Create one here!
+          </Link>
         </Box>
       </Box>
-    </Container>
+    </AuthCard>
   );
 }
--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -91,7 +91,7 @@ export default function Header() {
                   ))}
                 </Box>
 
-                <Button variant="contained" component={Link} href="/login">
+                <Button variant="contained" component={Link} href="/auth/login">
                   Log in
                 </Button>
               </>
