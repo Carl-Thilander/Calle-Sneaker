fb5886da5760420291b178c088dbbf4289ba5395
carl-thilander <carl.thilander@medieinstitutet.se>
2025-12-06 13:58:00 +0100

    Installed bcrypt and created first registration logic for new users. need to check if database is in sync and that passwords are being hashed.

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,15 +1,35 @@
+import { compare } from "bcrypt";
 import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
 import GithubProvider from "next-auth/providers/github";
+import { db } from "src/lib/db";
 
-export const authOptions = {
+const handler = NextAuth({
   providers: [
     GithubProvider({
-      clientId: process.env.GITHUB_ID ?? "",
-      clientSecret: process.env.GITHUB_SECRET ?? "",
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) return null;
+        const user = await db.user.findUnique({
+          where: { email: credentials.email },
+        });
+        if (!user) return null;
+        const valid = await compare(credentials.password, user.password);
+        return valid ? user : null;
+      },
     }),
   ],
-};
-
-export const handler = NextAuth(authOptions);
-
+  session: { strategy: "jwt" },
+  pages: {
+    signIn: "/auth/login",
+  },
+});
 export { handler as GET, handler as POST };
---
+++ src/app/api/auth/register/route.ts
@@ -0,0 +1,22 @@
+import { db } from "@/lib/db";
+import { hash } from "bcrypt";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+  const { email, password, name } = await req.json();
+  if (!email || !password) {
+    return NextResponse.json({ error: "Missing fields" }, { status: 400 });
+  }
+  const existingUser = await db.user.findUnique({ where: { email } });
+  if (existingUser) {
+    return NextResponse.json(
+      { error: "email already registred" },
+      { status: 409 }
+    );
+  }
+  const hashedPassword = await hash(password, 10);
+  const user = await db.user.create({
+    data: { email, name, password: hashedPassword },
+  });
+  return NextResponse.json({ user: { id: user.id, email: user.email } });
+}
