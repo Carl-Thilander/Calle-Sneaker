747cdf0384eb49e5f84ea33e53c35dbc6e563091
carl-thilander <carl.thilander@medieinstitutet.se>
2025-12-08 13:54:45 +0100

    Small prisma changes

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,26 +1,14 @@
 import { PrismaClient } from "@prisma/client";
-import { withAccelerate } from "@prisma/extension-accelerate";
-import "dotenv/config";
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
+  prisma?: PrismaClient;
 };
 
-const createDb = () => {
-  const client = new PrismaClient({
-    log: ["query", "error", "warn"],
-    // Uncomment the next line after you have PRISMA_ACCELERATE_URL set
-    // accelerateUrl: process.env.PRISMA_ACCELERATE_URL!,
+export const db =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ["error", "warn"],
   });
-
-  // Only attach Accelerate when an accelerateUrl is available
-  if (process.env.PRISMA_ACCELERATE_URL) {
-    return client.$extends(withAccelerate()) as unknown as PrismaClient;
-  }
-  return client;
-};
-
-export const db = globalForPrisma.prisma ?? createDb();
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = db;
