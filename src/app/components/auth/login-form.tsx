17e90887c2cc334753291255838e380b2f3c56d3
Carl Thilander <carl.thilander@medieinstitutet.se>
2025-12-09 11:02:08 +0100

    Created simple login logic with signIn and logout with signOut. Users can now register and login easily. Will need to implement zod to both later.

--- src/app/components/auth/login-form.tsx
+++ src/app/components/auth/login-form.tsx
@@ -1,7 +1,35 @@
-import { Box, Button, Container, Link, TextField } from "@mui/material";
-import AuthCard from "./AuthCard";
+"use client";
 
-export default async function LoginForm() {
+import { Box, Button, Container, Link, TextField, Typography } from "@mui/material";
+import AuthCard from "./AuthCard";
+import React, { useState } from "react";
+import { signIn } from "next-auth/react";
+
+export default function LoginForm() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState<string | null>(null);
+  const [loading, setLoading] = useState(false);
+
+  async function handleSubmit(e: React.FormEvent<HTMLElement>) {
+    e.preventDefault();
+    setError(null);
+    setLoading(true);
+
+    const result = await signIn("credentials", {
+      email,
+      password,
+      redirect: false,
+    });
+    setLoading(false);
+
+    if (result?.error) {
+      setError("Incorrect email or password");
+      return;
+    }
+
+    window.location.href="/profile";
+  }
   return (
     <Container
       maxWidth="lg"
@@ -12,11 +40,18 @@ export default async function LoginForm() {
       <AuthCard title="Welcome Back!" subtitle="Please log in to your account">
         <Box
           component="form"
+          onSubmit={handleSubmit}
           sx={{ display: "flex", flexDirection: "column", gap: 2, mt: 2 }}
         >
-          <TextField label="Email" type="email" fullWidth required />
-          <TextField label="Password" type="password" fullWidth required />
+          <TextField label="Email" type="email" fullWidth required value={email} onChange={e => setEmail(e.target.value)} />
+          <TextField label="Password" type="password" fullWidth required value={password} onChange={e => setPassword(e.target.value)} />
 
+
+          {error && (
+            <Typography color="error" variant="body2">
+              {error}
+            </Typography>
+          )}
           <Button
             type="submit"
             variant="contained"
@@ -24,12 +59,13 @@ export default async function LoginForm() {
             fullWidth
             sx={{ mt: 2, borderRadius: 5 }}
           >
-            Log In
+            {loading ? "Logging in..." : "Log in"}
+            
           </Button>
 
           <Box textAlign="center" mt={2}>
             <Link href="/auth/register" style={{ textDecoration: "none" }}>
-              Dont't have an account yet? Create one here!
+              Dont have an account yet? Create one here!
             </Link>
           </Box>
         </Box>
