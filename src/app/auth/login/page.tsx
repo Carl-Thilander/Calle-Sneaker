8416ca12f1d714393162c51163bb8b68b8a1c4e5
carl-thilander <carl.thilander@medieinstitutet.se>
2025-12-20 12:55:01 +0100

    Changed imports in register and login apge

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,5 +1,6 @@
-import AuthCard from "@/app/components/Auth/AuthCard";
-import LoginForm from "@/app/components/Auth/login-form";
+import AuthCard from "@/app/components/auth/AuthCard";
+import LoginForm from "@/app/components/auth/login-form";
+
 import { Typography } from "@mui/material";
 import Link from "next/link";
 
